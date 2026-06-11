a92d3e995a96ae1b7481198af7e07889223072a5
Maya Atanasova <mayaatanasova@gmail.com>
2026-06-11 19:47:10 +0300

    allow anydesk tcp

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -113,6 +113,8 @@ sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw allow ssh
 sudo ufw allow 'Nginx Full'
+sudo ufw allow 7070/tcp
+sudo ufw allow 7070/udp
 sudo ufw --force enable
 sudo ufw status verbose
 
